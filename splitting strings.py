155a2f9a1a3d33026ad9092afcb49bb1f910fc58
Badr El Gezeri <99209046+BadrElGezeri@users.noreply.github.com>
2022-10-24 18:00:24 +0200

    2nd commit
    Added writing to files and reading from files

--- splitting strings.py
+++ splitting strings.py
@@ -1,37 +1,68 @@
 print("Booting up...")
-print("Welcome to Encrypto \nPress any key to start\n")
+input("Welcome to Encrypto \nPress any key to start\n")
 
 def key(x):
     y = x + 3 # A random function to scramble the ASCII values
     return y # Return the result of the function
 
-
-from math import sqrt #import square root function from math libary
-
 def unkey(x):
     y = x - 3
     return y
 
+def listToString(s):
+ 
+    # initialize an empty string
+    str1 = ""
+ 
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+ 
+    # return string
+    return str1
 
-string = input("Input a string: \n")
-list = ([*string])
-print(list)
-length = len(string)
-count = 0
 new_list = []
 dec_list = []
+
 state = str(input("Do you want to Encrypt or Decrypt? "))
 
 if state == "Encrypt" or state == "encrypt":
+    string = input("Input a string: \n")
+    list = ([*string])
+    # print(list)
+    length = len(string)
     for i in range(length):
         # print(ord(list[i]))
         new_list.append(key(ord(list[i])))
         dec_list.append(chr(new_list[i]))
-    print("".join(map(str,dec_list)))
+    print("".join(map(str,dec_list)), end = " - is the encrypted form\n")
+    done = listToString(dec_list)
+    file = input("Do you want to save the encrypted form in a file? ")
+    if file == "Yes" or file == "yes":
+        with open('encrypted.txt', 'w') as f:
+            f.write(str(done))
 
 if state == "Decrypt" or state == "decrypt":
-    size = int(input("How many values do you have?"))
-    for i in range(size):
-        dec_list.append(int(input("Enter the values of the list: ")))
-        new_list.append(chr(unkey(dec_list[i])))
-    print("".join(map(str,new_list)))
+    file = input("Do you want to decrypt by inputting a string or form a file? ")
+    if file == "file" or file == "File":
+        with open('E:\encrypted.txt') as f:
+            string = f.readlines()
+        list = ([*string])
+        print(list)
+        list.append(list)
+        length = len(string)
+        for i in range(length):
+            new_list.append(unkey(ord(list[i])))
+            dec_list.append(chr(new_list[i]))
+        print("".join(map(str,dec_list)), end = " - is the decrypted form")
+    else:
+        string = input("Input a string\n")
+        list = ([*string])
+        length = len(string)
+        for i in range(length):
+            new_list.append(unkey(ord(list[i])))
+            dec_list.append(chr(new_list[i]))
+        print("".join(map(str,dec_list)), end = " - is the decrypted form")
+input("\nPress any key to close")
+
+
