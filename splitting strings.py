527b8fb517445ebd848dffbc3e275c424b1730c0
Badr El Gezeri <99209046+BadrElGezeri@users.noreply.github.com>
2022-10-24 20:03:11 +0200

    DONEEEEEEEEEEEEEE
    Just need to add some comments
    
    Make the code look a bit more tidy

--- splitting strings.py
+++ splitting strings.py
@@ -1,7 +1,34 @@
 import time
+import random
 from pathlib import Path
 import sys
 from time import sleep
+def listToString(s):
+ 
+    # initialize an empty string
+    str1 = ""
+ 
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+ 
+    # return string
+    return str1
+x= []
+for i in range(4):
+    x.append(4+(i-1)*2)
+
+with open("Keys.txt", 'w') as f:
+    f.writelines(str(x))
+
+with open("Keys.txt") as f:
+    rand_num = f.read()
+listToString(rand_num)
+rand_num = rand_num.replace(" ","")
+rand_num = rand_num.replace("[","")
+rand_num = rand_num.replace("]","")
+rand_num = rand_num.replace(",","")
+
 words = "Booting up..."
 for char in words:
     sleep(0.2)
@@ -15,96 +42,98 @@ for char in words2:
 
 input("\nPress any key to start\n")
 def key(x):
-    y = x + 3 # A random function to scramble the ASCII values
+    y = x + key_num # A random function to scramble the ASCII values
     return y # Return the result of the function
 
-def unkey(x):
-    y = x - 3 #Reverses what the key() function does
+def unkey(x,user_key):
+    y = x - user_key #Reverses what the key() function does
     return y 
 
-def listToString(s):
- 
-    # initialize an empty string
-    str1 = ""
- 
-    # traverse in the string
-    for ele in s:
-        str1 += ele
- 
-    # return string
-    return str1
+
 
 new_list = []
 dec_list = []
 
-state = str(input("Do you want to Encrypt or Decrypt? ")).lower()
 
 
-if state == "encrypt":
-    choice = input("Do you want to encrypt a string or a file? ").lower()
-    if choice == "string":
-        string = input("Input a string: \n")
-        list = ([*string])
-        # print(list)
-        length = len(string)
-        for i in range(length):
-            # print(ord(list[i]))
-            new_list.append(key(ord(list[i])))
-            dec_list.append(chr(new_list[i]))
-        print("".join(map(str,dec_list)), end = " - is the encrypted form\n")
-        done = listToString(dec_list)
-    elif choice == "file":
-        file_name = str(input("Please enter the file path: "))
-        file_name = file_name.replace("\"","")
-        with open(file_name) as f:
-            string = f.read()
-        print("The file contains the string:",string)
-        list = ([*string])
-        # print(list)
-        length = len(string)
-        for i in range(length):
-            # print(ord(list[i]))
-            new_list.append(key(ord(list[i])))
-            dec_list.append(chr(new_list[i]))
-        done = listToString(dec_list)
-        print(done, "is the encrypted form\n")
-    file = input("Do you want to save the encrypted form in a file? ")
-    if file == "Yes" or file == "yes":
-        timestr = time.strftime("_%d-%m-%Y_%H-%M-%S")
-        filename_saved = "encrypted" + str(timestr) + ".txt"
-        with open(filename_saved, 'w') as f:
-            f.write(str(done))
-        print("File saved as", filename_saved,"at", Path.cwd())
-if state == "decrypt":
-    choice = input("Do you want to decrypt a string or a file? ").lower()
-    if choice == "file":
-        file_name = str(input("Please enter the file path: "))
-        file_name = file_name.replace("\"","")
-        with open(file_name) as f:
-            string = f.read()
-            print("The file contains the string: ",string)
-            # print(type(string))
+while True:
+    state = str(input("Do you want to Encrypt or Decrypt?\n> ")).lower()
+    key_num = int(random.choice(rand_num))
+    if state == "encrypt":
+        choice = input("Do you want to encrypt text or a file?\n> ").lower()
+        if choice == "string" or choice == "text":
+            string = input("Input the text: \n> ")
+            list = ([*string])
+            # print(list)
+            length = len(string)
+            for i in range(length):
+                # print(ord(list[i]))
+                new_list.append(key(ord(list[i])))
+                dec_list.append(chr(new_list[i]))
+            print("".join(map(str,dec_list)), end = " - is the encrypted form\n")
+            print("The cryptography key = ", key_num)
+            done = listToString(dec_list)
+        elif choice == "file":
+            file_name = str(input("Please enter the file path: \n> "))
+            file_name = file_name.replace("\"","")
+            with open(file_name) as f:
+                string = f.read()
+            print("The file contains the string:",string)
+            list = ([*string])
+            # print(list)
+            length = len(string)
+            for i in range(length):
+                # print(ord(list[i]))
+                new_list.append(key(ord(list[i])))
+                dec_list.append(chr(new_list[i]))
+            done = listToString(dec_list)
+            print(done, "is the encrypted form\n")
+        file = input("Do you want to save the encrypted form in a file? \n> ")
+        if file == "Yes" or file == "yes":
+            timestr = time.strftime("_%d-%m-%Y_%H-%M-%S")
+            filename_saved = "encrypted" + str(timestr) + ".txt"
+            with open(filename_saved, 'w') as f:
+                f.write(str(done))
+            print("File saved as", filename_saved,"at", Path.cwd())
+    if state == "decrypt":
+        choice = input("Do you want to decrypt text or a file? \n> ").lower()
+        if choice == "file":
+            file_name = str(input("Please enter the file path: \n> "))
+            file_name = file_name.replace("\"","")
+            with open(file_name) as f:
+                string = f.read()
+                print("The file contains the string: ",string)
+                # print(type(string))
+                user_key = input("Enter the decryption key:\n> ")
+                list = ([*string])
+                length = len(string)
+                for i in range(length):
+                    new_list.append(unkey(ord(list[i])),user_key)
+                    dec_list.append(chr(new_list[i]))
+                done = listToString(dec_list)
+            print("".join(map(str,dec_list)), end = " - is the decrypted form\n")
+        elif choice == "string" or choice == "text":
+            string = input("Input the text\n> ")
+            user_key = int(input("Enter the decryption key:\n> "))
             list = ([*string])
             length = len(string)
             for i in range(length):
-                new_list.append(unkey(ord(list[i])))
+                new_list.append(unkey(ord(list[i]),user_key))
                 dec_list.append(chr(new_list[i]))
             done = listToString(dec_list)
         print("".join(map(str,dec_list)), end = " - is the decrypted form\n")
-    elif choice == "string":
-        string = input("Input a string\n")
-        list = ([*string])
-        length = len(string)
-        for i in range(length):
-            new_list.append(unkey(ord(list[i])))
-            dec_list.append(chr(new_list[i]))
-        done = listToString(dec_list)
-    print("".join(map(str,dec_list)), end = " - is the decrypted form\n")
-    file = input("Do you want to save the decrypted form in a file? ")
-    if file == "Yes" or file == "yes":
-        timestr = time.strftime("_%d-%m-%Y_%H-%M-%S")
-        filename_saved = "decrypted" + str(timestr) + ".txt"
-        with open(filename_saved, 'w') as f:
-            f.write(str(done))
-        print("File saved as", filename_saved,"at", Path.cwd())
+        file = input("Do you want to save the decrypted form in a file? \n> ")
+        if file == "Yes" or file == "yes":
+            timestr = time.strftime("_%d-%m-%Y_%H-%M-%S")
+            filename_saved = "decrypted" + str(timestr) + ".txt"
+            with open(filename_saved, 'w') as f:
+                f.write(str(done))
+            print("File saved as", filename_saved,"at", Path.cwd())
+    program_state = str(input("Do you want to retry?\n> ")).lower()
+    if program_state == "yes":
+        new_list = []
+        dec_list = []
+        continue
+    else:
+        break
 input("\nPress any key to close")
